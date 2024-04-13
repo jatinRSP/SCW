91659e98b24480ade66c5d17ec9d5b2e68bd7d95
Jatin Prajapati <JATINRSP575@GMAIL.COM>
2024-04-13 15:59:20 +0530

    Image ko import kiya rather then normal path

--- SCW/src/components/FrontPage/FrontPage.jsx
+++ SCW/src/components/FrontPage/FrontPage.jsx
@@ -1,33 +1,43 @@
 import React from "react";
 import Button from "../Button/Button";
-import "../../css/FrontPage.css"
+import "../../css/FrontPage.css";
+import Wtsp from "../../public/img/WTSP-100-BLACK.png";
 
-function FrontPage(){
-    return(
-        <section className="frontpage">
-            <article className="CenterContent">
-                <h1 className="headline">
-                    India's #1
-                    <br/>
-                    <ul>
-                        <li style={{"--chr": 16,"--delay":".5s"}}>Painting Service</li>    
-                        <li style={{"--chr": 16,"--delay":".2s"}}>Painting Product</li>    
-                        <li style={{"--chr": 17,"--delay":".3s"}}>Painting Building</li>    
-                    </ul>
-                </h1>
-                <p className="subheadline">We provide the best painting service in India. We have a team of highly skilled painters who are experts in their field. We provide the best painting service in India. We have a team of highly skilled painters who are experts in their field.</p>
-                <div className="btns">
-                    <Button titletext="Call Now" classes="themed-btn btn"/>
-                    <Button titletext="Get Quote" classes="themed-btn btn"/>
-                </div>
-            </article>
-            <div className="floating-icon-whatsapp">
-                <a href="https://api.whatsapp.com/send/?phone=9824304318&text=Hello+Shree+Color+World+team%2C%0A%0ACould+you+please+provide+us+with+details+on+the+scope+of+work%2C+estimated+timeline%2C+pricing+breakdown%2C+and+any+additional+services+you+offer%3F+We%27re+particularly+interested+in+seeing+examples+of+your+previous+projects+to+assess+your+workmanship.%0A%0ALooking+forward+to+your+response.+Thank+you%21&type=phone_number&app_absent=0" target="_blank" rel="noreferrer">
-                    <img src="../../public/img/WTSP-100-BLACK.png" alt="" />
-                </a>
-            </div>
-        </section>
-    );
+function FrontPage() {
+  return (
+    <section className="frontpage">
+      <article className="CenterContent">
+        <h1 className="headline">
+          India's #1
+          <br />
+          <ul>
+            <li style={{ "--chr": 16, "--delay": ".5s" }}>Painting Service</li>
+            <li style={{ "--chr": 16, "--delay": ".2s" }}>Painting Product</li>
+            <li style={{ "--chr": 17, "--delay": ".3s" }}>Painting Building</li>
+          </ul>
+        </h1>
+        <p className="subheadline">
+          We provide the best painting service in India. We have a team of
+          highly skilled painters who are experts in their field. We provide the
+          best painting service in India. We have a team of highly skilled
+          painters who are experts in their field.
+        </p>
+        <div className="btns">
+          <Button titletext="Call Now" classes="themed-btn btn" />
+          <Button titletext="Get Quote" classes="themed-btn btn" />
+        </div>
+      </article>
+      <div className="floating-icon-whatsapp">
+        <a
+          href="https://api.whatsapp.com/send/?phone=9824304318&text=Hello+Shree+Color+World+team%2C%0A%0ACould+you+please+provide+us+with+details+on+the+scope+of+work%2C+estimated+timeline%2C+pricing+breakdown%2C+and+any+additional+services+you+offer%3F+We%27re+particularly+interested+in+seeing+examples+of+your+previous+projects+to+assess+your+workmanship.%0A%0ALooking+forward+to+your+response.+Thank+you%21&type=phone_number&app_absent=0"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={Wtsp} alt="" />
+        </a>
+      </div>
+    </section>
+  );
 }
 
 export default FrontPage;
--- SCW/src/components/Header/Header.jsx
+++ SCW/src/components/Header/Header.jsx
@@ -1,33 +1,47 @@
 import "../../css/Header.css";
 import React from "react";
-function Header(){
+import Logo from "../../public/logos/LOGO2.svg";
 
-
-
-    return (
-        <>  
-            <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap" rel="stylesheet"></link>
-            <header>
-
-                <div className="LogoWrapper">
-                    <img src="../../public/logos/LOGO2.svg" alt="Logo" />
-                </div>
-                <nav>
-                    <ul>
-                        <li style={{"--i":".1s"}}><a href="#">About US</a></li>
-                        <li style={{"--i":".2s"}}><a href="#">Services</a></li>
-                        <li style={{"--i":".3s"}}><a href="#">Pricing</a></li>
-                        <li style={{"--i":".4s"}}><a href="#">Contanct Us</a></li>
-                    </ul>
-                </nav>
-                <div className="hamburger" onClick={({target})=> {target.closest(".hamburger").classList.toggle("active")}}>
-                    <div className="line"></div>
-                    <div className="line"></div>
-                    <div className="line"></div>
-                </div>
-            </header>
-        </>
-    );
+function Header() {
+  return (
+    <>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap"
+        rel="stylesheet"
+      ></link>
+      <header>
+        <div className="LogoWrapper">
+          <img src={Logo} alt="Logo" />
+        </div>
+        <nav>
+          <ul>
+            <li style={{ "--i": ".1s" }}>
+              <a href="#">About US</a>
+            </li>
+            <li style={{ "--i": ".2s" }}>
+              <a href="#">Services</a>
+            </li>
+            <li style={{ "--i": ".3s" }}>
+              <a href="#">Pricing</a>
+            </li>
+            <li style={{ "--i": ".4s" }}>
+              <a href="#">Contanct Us</a>
+            </li>
+          </ul>
+        </nav>
+        <div
+          className="hamburger"
+          onClick={({ target }) => {
+            target.closest(".hamburger").classList.toggle("active");
+          }}
+        >
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
+      </header>
+    </>
+  );
 }
 
 export default Header;
