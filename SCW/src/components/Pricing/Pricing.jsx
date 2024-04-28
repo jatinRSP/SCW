d2ebbe7ea4eb87afd5b85dbf2de0587d54184b59
Jatin Prajapati <JATINRSP575@GMAIL.COM>
2024-04-28 12:21:29 +0530

    Changed the image path and imported images

--- SCW/src/components/Pricing/Pricing.jsx
+++ SCW/src/components/Pricing/Pricing.jsx
@@ -5,6 +5,21 @@ import "../../css/Pricing.css";
 import Pagination from "@mui/material/Pagination";
 import PaginationItem from "@mui/material/PaginationItem";
 import Stack from "@mui/material/Stack";
+import img1 from "../../../public/Products/1_ROYAL_PLASTIC_PAINT.jpeg";
+import img2 from "../../../public/Products/2_PREMIUM_EMULSION.jpg";
+import img3 from "../../../public/Products/3_APEX.jpg";
+import img4 from "../../../public/Products/4_PU.jpg";
+import img5 from "../../../public/Products/5_MELAMYNE.jpg";
+import img6 from "../../../public/Products/6_BIRLA.jpg";
+import img7 from "../../../public/Products/7_OIL.jpg";
+import img8 from "../../../public/Products/8_TEXTURE.jpg";
+import img9 from "../../../public/Products/9_MAGNUM.jpeg";
+import img10 from "../../../public/Products/10_ZINC.jpg";
+import img11 from "../../../public/Products/11_LAQUER.jpg";
+import img12 from "../../../public/Products/12_METALIC.jpg";
+import img13 from "../../../public/Products/13_EPOXY.jpg";
+import img14 from "../../../public/Products/14_DISTEMPER.webp";
+import img15 from "../../../public/Products/15_WALL_PRIMER.jpeg";
 
 function Pricing() {
   const [currentPage, setCurrentPage] = useState(1);
@@ -24,105 +39,105 @@ function Pricing() {
       {
         name: " Royal Plastic Paint",
         price: "₹32 / sq.ft",
-        image: "1_ROYAL_PLASTIC_PAINT.jpeg",
+        image: img1,
         desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       },
 
       {
         name: " Perfect Emulsion Paint",
         price: "₹25 / sq.ft",
-        image: "2_PREMIUM_EMULSION.jpg",
+        image: img2,
         desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       },
 
       {
         name: "Apex Paint",
         price: "₹14 / sq.ft",
-        image: "3_APEX.jpg",
+        image: img3,
         desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       },
 
       {
         name: "PU Paint",
         price: "₹400 / sq.ft",
-        image: "4_PU.jpg",
+        image: img4,
         desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       },
 
       {
         name: "Melamyne Polish",
         price: "₹100 / sq.ft",
-        image: "5_MELAMYNE.jpg",
+        image: img5,
         desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       },
 
       {
         name: "Birla Putty",
         price: "₹8 / sq.ft",
-        image: "6_BIRLA.jpg",
+        image: img6,
         desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       },
 
       {
         name: "Oil Paint",
         price: "₹20 / sq.ft",
-        image: "7_OIL.jpg",
+        image: img7,
         desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       },
 
       {
         name: "Rustic Texture",
         price: "₹50 / sq.ft",
-        image: "8_TEXTURE.jpg",
+        image: img8,
         desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       },
 
       {
         name: "Magnum Paint",
         price: "₹35 / sq.ft",
-        image: "9_MAGNUM.jpeg",
+        image: img9,
         desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       },
 
       {
         name: "Zinc Paint",
         price: "₹45 / sq.ft",
-        image: "10_ZINC.jpg",
+        image: img10,
         desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       },
 
       {
         name: "Laquer Paint",
         price: "₹35 / sq.ft",
-        image: "11_LAQUER.jpg",
+        image: img11,
         desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       },
 
       {
         name: "Metallic Paint",
         price: "₹80 TO 250 / sq.ft",
-        image: "12_METALIC.jpg",
+        image: img12,
         desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       },
 
       {
         name: "Epoxy Paint",
         price: "₹38 / sq.ft",
-        image: "13_EPOXY.jpg",
+        image: img13,
         desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       },
 
       {
         name: "Distemper Paint",
         price: "₹15 / sq.ft",
-        image: "14_DISTEMPER.webp",
+        image: img14,
         desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       },
 
       {
         name: "Wall Primer",
         price: "₹10 / sq.ft",
-        image: "15_WALL_PRIMER.jpeg",
+        image: img15,
         desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       },
     ];
@@ -131,7 +146,7 @@ function Pricing() {
       <div key={card} className="card">
         <div className="card-image">
           {/* Placeholder image */}
-          <img src={`../../../public/Products/${card.image}`} alt={card.name} />
+          <img src={card.image} alt={card.name} />
         </div>
         <div className="card-content">
           <h3 className="product-title">{card.name}</h3>
